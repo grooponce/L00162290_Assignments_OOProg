f808dd00cb738dc883b77343c6b9989bdf3750e1
Gonzalo Roo Ponce <l00162290@student.lyit.ie>
2021-11-08 21:03:40 +0000

    v1 beautiful soup

---
+++ l00162290_q2_beautifulsoup4.py
@@ -0,0 +1,10 @@
+"""
+# File:             l00162290_q2_beautifulsoup4.py
+# Created:          08/11/2021, 21:03
+# Author:           Gonzalo Roo Ponce
+# Student Number:   L00162290
+# Version:          1.0.0
+# Licensing:        GNU
+# Support Contact:  l00162290@student.lyit.ie
+# Comments:         
+"""
