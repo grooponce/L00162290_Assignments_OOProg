7d0594330053a1152c98b05fb9af66c9292d9726
Gonzalo Roo Ponce <l00162290@student.lyit.ie>
2021-12-06 13:56:39 +0000

    added BeautifulSoup script v1

--- Q2/L00162290_Q2_File_2.py
+++ Q2/L00162290_Q2_File_2.py
@@ -1,6 +1,6 @@
 """
-# File:             paramiko_test.py
-# Created:          02/11/2021, 09:34
+# File:             l00162290_q2_beautifulsoup4.py
+# Created:          08/11/2021, 21:03
 # Author:           Gonzalo Roo Ponce
 # Student Number:   L00162290
 # Version:          1.0.0
@@ -9,33 +9,12 @@
 # Comments:         
 """
 
-import paramiko
-import time
+from bs4 import BeautifulSoup
+import requests
 
-def ssh_connection():
-    global user_file
-    global cmd_file
-    global session
-    """ Define session globally as we're using both into the try instance but also to run commands """
+apache2 = 'http://192.168.1.169/'
+index_doc = requests.get(apache2)
 
-    try:
-        #open(user_file, 'r')
-        ip = "192.168.1.169"
-        user_name = "l00162290".rstrip("\n")
-        user_password = "J4ef3r34".rstrip("\n")
+soup = BeautifulSoup(index_doc.text, 'html.parser')
 
-        session = paramiko.SSHClient()
-        session.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-        """  this needed to be intended
-        ---> as function ssh_connection was not called
-        ---> and the variable session was needed inside the
-        ---> try instance """
-        session.connect(hostname=ip.rstrip("\n"),username=user_name,password=user_password)
-    except:
-        test = "test"
-
-""" Create file to verify SSH connectivity"""
-
-ssh_connection() # ---> function needed to be called to incorporate all the information collected """
-connection = session.invoke_shell()
-session.exec_command("mkdir This;cd This; touch that.txt\n")
+#print(soup.prettify())
